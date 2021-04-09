b21d7ee90eb8ebdd75c8828e18c6f27f5fb19ee4
Per Holte <phholte@stud.ntnu.no>
2021-04-09 18:41:36 +0200

    more fxml, connected the controller as well

--- project/src/main/java/tetris/App.java
+++ project/src/main/java/tetris/App.java
@@ -1,26 +1,20 @@
 package tetris;
-import javafx.application.Application; 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+
+import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setController(this);
         Parent root = (Parent) fxmlLoader.load(this.getClass().getResourceAsStream("App.fxml"));
         primaryStage.setTitle("TetrisJFX");
-        Scene scene = new Scene(root, 400, 510);
+        Scene scene = new Scene(root, 700, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
--- project/src/main/java/tetris/AppController.java
+++ project/src/main/java/tetris/AppController.java
@@ -1,5 +1,11 @@
 package tetris;
 
-public class AppController {
+import javafx.fxml.FXML;
 
+public class AppController {
+	
+	@FXML
+	public void handleStartGame() {
+		System.out.println("Start game!");	
+		}
 }
