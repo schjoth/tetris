e98a76e7d74bafdd23e8b8fe661fe70f010133a4
André <aschjoth@gmail.com>
2021-04-10 12:44:31 +0200

    lagde test for CoordinateController

--- project/src/main/java/coordinates/CoordinatesCalculator.java
+++ project/src/main/java/coordinates/CoordinatesCalculator.java
@@ -1,13 +1,13 @@
 package coordinates;
 
 public class CoordinatesCalculator {
-	public Coordinates calcuateCoordinates(int index, int columnLength) {
-		int y = index % columnLength;
-		int x = index - y;
+	public static Coordinates calcuateCoordinates(int index, int columnLength) {
+		int x = index % columnLength;
+		int y = index / columnLength;
 		return new Coordinates(x,y);
 	}
 	
-	public int calculateIndex(int posX, int posY, int columnLength) {
+	public static int calculateIndex(int posX, int posY, int columnLength) {
 		return posY * columnLength + posX;
 	}
 }
---
+++ project/src/test/java/coordinatesTests/CoordinatesTest.java
@@ -0,0 +1,32 @@
+package coordinatesTests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import coordinates.*;
+
+public class CoordinatesTest {
+	
+	@Test
+	public void testCalculateCoordinates() {
+		int index = 1;
+		int columnLength = 8;
+		Coordinates coo1 = CoordinatesCalculator.calcuateCoordinates(index, columnLength);
+		assertEquals(0, coo1.getY());
+		assertEquals(1, coo1.getX());
+		
+		int index2 = 46;
+		Coordinates coo2 = CoordinatesCalculator.calcuateCoordinates(index2, columnLength);
+		assertEquals(5, coo2.getY());
+		assertEquals(6, coo2.getX());
+	}
+	
+	@Test
+	public void testCalculateIndex() {
+		int x = 3;
+		int y = 5;
+		int columnLength = 10;
+		assertEquals(53, CoordinatesCalculator.calculateIndex(x, y, columnLength));
+	}
+}
