195ce773727d189647c40fc91d28592a3fa13255
Per Holte <phholte@stud.ntnu.no>
2021-04-10 11:32:38 +0200

    small changes

--- project/src/main/java/tetris/AppController.java
+++ project/src/main/java/tetris/AppController.java
@@ -5,7 +5,21 @@ import javafx.fxml.FXML;
 public class AppController {
 	
 	@FXML
+	public void onStart() {
+		loadHighScores();
+	}
+	
+	@FXML
 	public void handleStartGame() {
 		System.out.println("Start game!");	
 		}
+	
+	@FXML
+	public void loadHighScores() {
+		
+	}
+	
+	
+	
+	
 }
