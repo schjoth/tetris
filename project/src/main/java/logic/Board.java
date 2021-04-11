9e54979a38e1dad9d61b6a1b8ecd5832a8e38739
André <aschjoth@gmail.com>
2021-04-11 11:59:47 +0200

    la til noen grunnelementer i board.java

--- project/src/main/java/logic/Board.java
+++ project/src/main/java/logic/Board.java
@@ -1,6 +1,33 @@
 package logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import shape.Shape;
+
 public class Board {
+	
+	private int posX, posY, startPosX, startPosY;
+	
+	//hver block blir representert ved fargen sin som en string
+	private List<List<String>> board = new ArrayList<>();
+	
+	public Board(int columnLength, int rowLength) {
+		startPosY = 0;
+		startPosX = columnLength / 2;
+		posX = startPosX;
+		posY = startPosY;
+		
+		while (board.size() < rowLength) {
+			List<String> list = new ArrayList<>();
+			while (list.size() < columnLength) {
+				list.add(null);
+			}
+			board.add(list);
+		}
+		
+		System.out.println(board);
+	}
 
 	/*	
 	for (index : getShapeIndexes()) {
@@ -8,5 +35,38 @@ public class Board {
 		GridPane.add(coo.getX())
 	}
 	*/
-
+	
+	public void moveRight() {
+		posX++;
+	}
+	
+	public void moveLeft() {
+		posX--;
+	}
+	
+	public void moveDown() {
+		//TODO sjekk om den treffer noe under
+		posY++;
+	}
+	
+	public void insertNewBlock(Shape shape) {
+		posX = startPosX;
+		posY = startPosY;
+	}
+	
+	public int getPosX() {
+		return posX;
+	}
+	
+	public int getPosY() {
+		return posY;
+	}
+	
+	public int getColumnLength() {
+		return board.get(0).size();
+	}
+	
+	public int getRowLength() {
+		return board.size();
+	}
 }
---
+++ project/src/main/java/logic/HandleShapeRotate.java
@@ -0,0 +1,14 @@
+package logic;
+
+import shape.Shape;
+
+public class HandleShapeRotate {
+	public static boolean tryRotateRight(Shape shape, Board board) {
+		int posX = board.getPosX();
+		int posY = board.getPosY();
+		
+		
+		
+		return false;
+	}
+}
